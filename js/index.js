3ef7c1834cc6dbd07689b5241371d70c5d282812
Anastasia Lalamentik <anastasia.lalamentik@alumni.unc.edu>
2020-04-22 00:50:58 -0400

    need to add calibration

--- js/index.js
+++ js/index.js
@@ -12,6 +12,15 @@ app.listen(3000, () => {
 const { MUSICAL_NOTES_MAP } = require('./musicalNotesMapping') //To get the user friendly form of the musical notes returned
 const frequencyMap = require('note-frequency-map')
 let lightMode = 0
+let lowerRange = 800
+let upperRange = 2000
+let incrementalValue = (upperRange - lowerRange) / 10
+let frequencyLightLevels = []
+for (var i = lowerRange; i <= upperRange; i += incrementalValue) {
+    frequencyLightLevels.push(i)
+}
+
+console.log(frequencyLightLevels)
 
 app.get("/lightStatus", (req, res, next) => {
     res.json({"status" : lightMode })
@@ -32,15 +41,15 @@ fs.readFile('./index.html', function (err, html) {
 /* Phyphox configuration. If using Mac, get rid of the :8080 at the end. 
 If using Windows, add :8080 at the end. The PP_ADDRESS differs for each person, 
 and can be found on the phyphox app on your phones */
-const PP_ADDRESS = "http://192.168.0.6" 
+const PP_ADDRESS = "http://192.168.0.143" 
 const PP_CHANNELS = ["frequency", "semitonesRound", "centsDiff"]  // Names of the buffers in the phyphox experiement we want to pull data from
 const sampling_rate = 100  // Sampling at 100 Hz
 
 // Animation and data collection config
 const PREV_SAMPLE = 100                   // Size of the buffer of past data that we want to display in the graph
 const INTERVALS = 1000 / sampling_rate    // Interval to delay between data collection repetitions
-const WHISTLE_MAX_FREQ = 3000  // Maximum frequency in whistling range
-const WHISTLE_MIN_FREQ = 400  // Minimum frequency in whistling range
+const WHISTLE_MAX_FREQ = 5000  // Maximum frequency in whistling range
+const WHISTLE_MIN_FREQ = 500  // Minimum frequency in whistling range
 
 // Global var to save timestamp
 let xs = []
@@ -74,7 +83,8 @@ let getData = async() => {
         peak_frequencies.push([date.getTime(), temp_peak_frequency])
         musical_notes.push([date.getTime(), temp_musical_note])
         cents_from_notes.push([date.getTime(), temp_cents_from_note])
-        lightMode = 1
+        lightMode = determineLightMode(temp_peak_frequency)
+        console.log('light mode', lightMode)
         // this node library accurately maps frequencies to musical notes, an option we can use and omit later if not needed
         console.log(myNote.note) 
         console.log(date.getTime(), temp_peak_frequency, MUSICAL_NOTES_MAP[temp_musical_note], temp_cents_from_note)
@@ -83,6 +93,32 @@ let getData = async() => {
         lightMode = 0
         console.log("Error: " + err.message)
     })
+}
+
+let determineLightMode = (frequency) => {
+    if (frequency <= frequencyLightLevels[0]) {
+        return 0;
+    } else if (frequency <= frequencyLightLevels[1]) {
+        return 1;
+    } else if (frequency <= frequencyLightLevels[2]) {
+        return 2;
+    } else if (frequency <= frequencyLightLevels[3]) {
+        return 3;
+    } else if (frequency <= frequencyLightLevels[4]) {
+        return 4;
+    } else if (frequency <= frequencyLightLevels[5]) {
+        return 5;
+    } else if (frequency <= frequencyLightLevels[6]) {
+        return 6;
+    } else if (frequency <= frequencyLightLevels[7]) {
+        return 7;
+    } else if (frequency <= frequencyLightLevels[8]) {
+        return 8;
+    } else if (frequency <= frequencyLightLevels[9]) {
+        return 9;
+    } else {
+        return 10;
+    }
 }
 
 setInterval(() => {
