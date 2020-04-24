452e609a234e43a2c6f292ee2fcbb54010bcefed
Anastasia Lalamentik <anastasia.lalamentik@alumni.unc.edu>
2020-04-23 21:37:12 -0400

    disco lights

--- index.js
+++ index.js
@@ -18,15 +18,7 @@ app.use(bodyParser.raw());
 const { MUSICAL_NOTES_MAP } = require('./musicalNotesMapping') //To get the user friendly form of the musical notes returned
 const frequencyMap = require('note-frequency-map')
 let lightMode = 0
-let lowerRange = 800
-let upperRange = 2000
-let incrementalValue = (upperRange - lowerRange) / 10
 let frequencyLightLevels = []
-for (var i = lowerRange; i <= upperRange; i += incrementalValue) {
-    frequencyLightLevels.push(i)
-}
-
-console.log(frequencyLightLevels)
 
 /* Phyphox configuration. If using Mac, get rid of the :8080 at the end. 
 If using Windows, add :8080 at the end. The PP_ADDRESS differs for each person, 
@@ -41,15 +33,33 @@ app.get("/lightStatus", (req, res, next) => {
 })
 
 app.post('/setPPAddress',(req, res) => {
-    PP_ADDRESS = req.body["address"]
+    PP_ADDRESS = req.body.address
+    console.log(PP_ADDRESS)
     url = PP_ADDRESS + "/get?" + PP_CHANNELS.join("&")
+    res.send('Success.')
 })
+
+app.post('/setRange',(req, res) => {
+    let lowerRange = req.body.lowerRange
+    let upperRange = req.body.upperRange
+    res.send('Success.')
+    frequencyLightLevels = []
+    let range = (upperRange - lowerRange)
+    let levelCount = 10
+    let incrementalValue = parseFloat(range / levelCount)
+    for (var i = 0; i < 10; i ++) {
+        frequencyLightLevels.push(parseFloat(lowerRange) + parseFloat(incrementalValue*i))
+    }
+    console.log(frequencyLightLevels)
+})
+
+
 
 // Animation and data collection config
 const PREV_SAMPLE = 100                   // Size of the buffer of past data that we want to display in the graph
 const INTERVALS = 1000 / sampling_rate    // Interval to delay between data collection repetitions
 const WHISTLE_MAX_FREQ = 5000  // Maximum frequency in whistling range
-const WHISTLE_MIN_FREQ = 500  // Minimum frequency in whistling range
+const WHISTLE_MIN_FREQ = 100  // Minimum frequency in whistling range
 
 // Global var to save timestamp
 let xs = []
@@ -85,18 +95,17 @@ let getData = async() => {
         lightMode = determineLightMode(temp_peak_frequency)
         console.log('light mode', lightMode)
         // this node library accurately maps frequencies to musical notes, an option we can use and omit later if not needed
-        console.log(myNote.note) 
-        console.log(date.getTime(), temp_peak_frequency, MUSICAL_NOTES_MAP[temp_musical_note], temp_cents_from_note)
+       // console.log(myNote.note) 
+       console.log(date.getTime(), temp_peak_frequency, MUSICAL_NOTES_MAP[temp_musical_note], temp_cents_from_note)
     })
     }).on("error", (err) => {
         lightMode = 0
-        PP_ADDRESS = "http://192.168.0.143:8080" 
         console.log("Error: " + err.message)
     })
 }
 
 let determineLightMode = (frequency) => {
-    if (frequency <= frequencyLightLevels[0]) {
+    if (frequencyLightLevels.length < 10 || frequency <= frequencyLightLevels[0]) {
         return 0;
     } else if (frequency <= frequencyLightLevels[1]) {
         return 1;
